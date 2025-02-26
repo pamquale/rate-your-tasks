96541c255277c69a8815b1acafafe6899e4da85e
denis <denisruzhynski3@gmail.com>
2025-02-26 18:32:28 +0200

    Update main

--- core/main/urls.py
+++ core/main/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('login', views.login),
-    path('registration', views.registration)
+    path('', views.Home, name="home"),
+    path('login/', views.LoginView, name="login"),
+    path('register/', views.RegisterView, name="register"),
+    path('logout/', views.LogoutView, name='logout'),
+    path('forgot-password/', views.ForgotPassword, name='forgot-password'),
+    path('password-reset-sent/<str:reset_id>/', views.PasswordResetSent, name='password-reset-sent'),
+    path('reset-password/<str:reset_id>/', views.ResetPassword, name='reset-password'),
 ]
